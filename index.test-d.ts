17a7e95610dc89793035ede0cfa3cbc4ac29a969
fisker <lionkay@gmail.com>
2025-11-24 20:31:46 +0800

    Assert tags

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,6 +4,7 @@ import yamlTestSuite from './index.js'
 for (const file of yamlTestSuite) {
   expectAssignable<string>(file.id)
   expectAssignable<string>(file.name)
+  expectAssignable<string[]>(file.tags)
 
   for (const testCase of file.cases) {
     expectAssignable<string>(testCase.yaml)
